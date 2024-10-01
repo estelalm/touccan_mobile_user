2ee99e9504d6a8e40a6b31f4d709b19e2b1d4cd0
ESTELA ALVES DE MORAES <23111781@dsn127m1242373.cfp127aluno.local>
2024-10-01 10:43:30 -0300

    01 out. 2024 - Acesso ao localhost

--- app/src/main/java/br/senai/sp/jandira/touccanuser/service/RetrofitFactory.kt
+++ app/src/main/java/br/senai/sp/jandira/touccanuser/service/RetrofitFactory.kt
@@ -7,7 +7,7 @@ class RetrofitFactory {
 
     private val BASE_URL = "https://touccan-backend-8a78.onrender.com/2.0/touccan/"
 
-//    private val BASE_URL = "http://localhost:8080/2.0/tocuccan/"
+//    private val BASE_URL = "http://10.0.2.2:8080/2.0/tocuccan/"
 
     //cliente http
     private val retrofitFactory = Retrofit
